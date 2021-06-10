1e908b78c06be9121fb91140554441e68d089aae
89trillion-zhuanlei <anlei@89trillion.com>
2021-06-10 16:38:15 +0800

    first commit

---
+++ Assets/Scripts/StartUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartUI : MonoBehaviour
+{
+    public GameObject game;
+    public Button startBtn;
+
+    public void ClickStart()
+    {
+        game.SetActive(true);
+        startBtn.gameObject.SetActive(false);
+    }
+}
